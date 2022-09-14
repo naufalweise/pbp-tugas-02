f3962c2fd6d668f930df111703063440908e0a66
Naufal Weise <naufalwwidyatama@gmail.com>
2022-09-14 11:29:56 +0700

    Add tugas 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,8 @@
-# TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import index
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', index, name='index'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import CatalogItem
 
-# TODO: Create your views here.
+STUDENT_NAME = "NAUFAL WEISE WIDYATAMA"
+STUDENT_ID = "2106750263"
+
+def index(request):
+    catalog_items = CatalogItem.objects.all()
+    context = {'catalog_items': catalog_items, 'student_name': STUDENT_NAME, 'student_id': STUDENT_ID}
+    return render(request, 'katalog.html', context)
