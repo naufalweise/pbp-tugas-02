4de3d2c2dfb7575972419b1becc67c5733a3b666
Naufal Weise <naufalwwidyatama@gmail.com>
2022-09-22 04:19:27 +0700

    Add unit test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import MyWatchList
+import datetime
 
-# Create your tests here.
+class MyWatchListTestCase(TestCase):
+    def setUp(self):
+        MyWatchList.objects.create(title="Movie 1", rating=3, release_date=datetime.date.today(), review = "Review 1")
+        MyWatchList.objects.create(title="Movie 2", rating=3, release_date=datetime.date.today(), review = "Review 2")
+
+    def test_endpoints(self):
+        """Test tiap endpoint urls memiliki status code 200"""
+        client = Client()
+
+        response = client.get("/mywatchlist/json/")
+        self.assertEqual(response.status_code, 200)
+        response = client.get("/mywatchlist/xml/")
+        self.assertEqual(response.status_code, 200)
+        response = client.get("/mywatchlist/html/")
+        self.assertEqual(response.status_code, 200)
