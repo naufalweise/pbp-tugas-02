7f03278f611215221c1c473144c42f89e1f49c7e
Naufal Weise <naufalwwidyatama@gmail.com>
2022-09-22 05:04:25 +0700

    Fix error collectstatic

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py loaddata initial_catalog_data.json
 python manage.py loaddata initial_mywatchlist_data.json
-python manage.py collectstatic
+python manage.py collectstatic --noinput
