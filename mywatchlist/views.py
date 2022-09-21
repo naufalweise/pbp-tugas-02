41a87be66422b45056405a810d1cef4e8fe37b8a
naufalweise <naufalwwidyatama@gmail.com>
2022-09-21 07:11:44 +0700

    Add json & xml implementation

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
+from .models import MyWatchList
+from django.core import serializers
 
 def index(request):
     return HttpResponse("Hello Watchlist")
 
+
 def html(request):
     return HttpResponse("Hello html")
+
+
 def json(request):
-    return HttpResponse("Hello json")
+    return HttpResponse(serializers.serialize("json", MyWatchList.objects.all()), content_type="application/json")
+
+
 def xml(request):
-    return HttpResponse("Hello xml")
+    return HttpResponse(serializers.serialize("xml", MyWatchList.objects.all()), content_type="application/xml")
