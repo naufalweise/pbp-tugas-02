a90a0a0b4fad987435112a8cf13ec8d7cca172b6
naufalweise <naufalwwidyatama@gmail.com>
2022-09-27 00:00:16 +0700

    Add task model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,10 @@
+from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+	user = models.ForeignKey(User, models.CASCADE)
+	date = models.DateField()
+	title = models.CharField(max_length=255)
+	description = models.TextField()
