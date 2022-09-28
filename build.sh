c0264dcd41707f1a153bd6ba78d69ec5b7da073b
Naufal Weise <naufalwwidyatama@gmail.com>
2022-09-28 09:20:28 +0700

    Add dummy data

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 python manage.py migrate
 python manage.py loaddata initial_catalog_data.json
 python manage.py loaddata initial_mywatchlist_data.json
+python manage.py loaddata init-todolist-user.json
+python manage.py loaddata init-todolist-task.json
 python manage.py collectstatic --noinput
